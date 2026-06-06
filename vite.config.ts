e727782dc0bcb89ffec3c7b91dd27c6263b9a808
Guilherme <guilhermefortunato671@gmail.com>
2026-06-05 21:53:05 -0300

    fix: remove vite-prerender-plugin after errors during yarn build & add fallback to index.html for SEO

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,11 @@ import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
-import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    vitePrerenderPlugin({
-      renderTarget: '#root',
-      prerenderScript: path.resolve(__dirname, 'src/prerender.tsx'),
-    })
   ],
   resolve: {
     alias: {
