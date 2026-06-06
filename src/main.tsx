68412161eefe04d3229f9f37727c9693d37f72b4
Guilherme <guilhermefortunato671@gmail.com>
2026-06-05 21:28:31 -0300

    feat: add vite-prerender-ssg plugin and i18n Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import { Cursor } from "./components/Cursor";
+import { ReactLenis } from 'lenis/react';
+import 'lenis/dist/lenis.css'
+import "@/i18n";
 
 const rootElement = document.getElementById("root");
 
@@ -11,6 +15,17 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
+    <ReactLenis 
+      root
+      options={{
+        autoRaf: true,
+        anchors: true,
+        duration: 2.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true,
+      }}
+      />
+    <Cursor />
     <App />
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    vitePrerenderPlugin({
+      renderTarget: '#root',
+      prerenderScript: path.resolve(__dirname, 'src/prerender.tsx'),
+    })
   ],
   resolve: {
     alias: {
